e0b836d4f7e62eac62b9dc8ba633d06fcdb9d0ad
Tom <larsenthomasj@gmail.com>
2020-04-23 06:48:15 -0400

    compiles, but not sure if it works

--- goalteryx/glue.go
+++ goalteryx/glue.go
@@ -2,18 +2,34 @@ package main
 
 /*
 #include "plugins.h"
-#include "engine.h"
-
-extern __declspec(dllexport) long _stdcall AlteryxGoPlugin(int nToolID,
-	const wchar_t *pXmlProperties,
-	struct EngineInterface *pEngineInterface,
-	struct PluginInterface *r_pluginInterface)
-{
-
-};
 */
 import "C"
+import (
+	"github.com/mattn/go-pointer"
+	"unicode/utf16"
+	"unsafe"
+)
 
 func main() {
 
+}
+
+type Plugin interface {
+	PushAllRecords(recordLimit int) int
+}
+
+//export PiPushAllRecords
+func PiPushAllRecords(handle unsafe.Pointer, recordLimit C.__int64) C.long {
+	alteryxPlugin := pointer.Restore(handle).(Plugin)
+	return C.long(alteryxPlugin.PushAllRecords(int(recordLimit)))
+}
+
+func UTF16ToString(s []uint16) string {
+	for i, v := range s {
+		if v == 0 {
+			s = s[0:i]
+			break
+		}
+	}
+	return string(utf16.Decode(s))
 }
--- goalteryx/plugins.h
+++ goalteryx/plugins.h
@@ -1,4 +1,7 @@
 #include <stdbool.h>
+#include <stddef.h>
+
+// Plugin definitions
 
 struct RecordData
 {
@@ -41,3 +44,27 @@ struct PluginInterface
 	T_PI_AddOutgoingConnection		pPI_AddOutgoingConnection;
 	T_PI_PushAllRecords				pPI_PushAllRecords;
 };
+
+// Engine definitions
+
+typedef void AlteryxThreadProc(void *pData);
+struct PreSortConnectionInterface;
+struct EngineInterface {
+    const wchar_t (*CreateTempFileName)(const wchar_t *pExt);
+    const wchar_t (*CreateTempFileName2)(const wchar_t *pExt, int nOptions);
+    const wchar_t (*GetInitVar)(const wchar_t *pVar);
+    int (*IsLicensed)(const wchar_t *pDll, const wchar_t *pEntryPoint);
+    long (*OutputMessage)(int nToolID, int nStatus, const wchar_t *pMessage);
+    long (*OutputToolProgress)(int nToolID, double dPercentProgress);
+    long (*PreSort)(int nToolID, const wchar_t *pSortInfo, struct IncomingConnectionInterface *pOrigIncConnInt, struct IncomingConnectionInterface ** r_ppNewIncConnInt, struct PreSortConnectionInterface ** r_ppPreSortConnInt);
+    void (*QueueThread)(AlteryxThreadProc pProc, void *pData);
+};
+
+// For the glue
+
+long PiPushAllRecords(void * handle, __int64 nRecordLimit);
+
+long _stdcall AlteryxGoPlugin(int nToolID,
+	const wchar_t *pXmlProperties,
+	struct EngineInterface *pEngineInterface,
+	struct PluginInterface *r_pluginInterface);
