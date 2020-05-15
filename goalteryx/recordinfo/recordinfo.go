f1318b79d46b06c2bc8289002e569531d29d4800
Tom <larsenthomasj@gmail.com>
2020-05-15 06:45:33 -0400

    Let's be more explicit about how much of a field's fixed size is storage for the actual value vs how much is used for the null indicator.

--- goalteryx/recordinfo/recordinfo.go
+++ goalteryx/recordinfo/recordinfo.go
@@ -57,12 +57,14 @@ var DateType = `date`
 var DateTimeType = `datetime`
 
 type FieldInfo struct {
-	Name      string
-	Source    string
-	Size      int
-	Precision int
-	Type      string
-	location  uintptr
+	Name        string
+	Source      string
+	Size        int
+	Precision   int
+	Type        string
+	location    uintptr
+	fixedLen    uintptr
+	nullByteLen uintptr
 }
 
 func New() RecordInfo {
@@ -81,59 +83,59 @@ func (info *recordInfo) GetFieldByIndex(index int) (FieldInfo, error) {
 }
 
 func (info *recordInfo) AddByteField(name string, source string) string {
-	return info.addField(name, source, 1, 0, ByteType, 2)
+	return info.addField(name, source, 1, 0, ByteType, 1, 1)
 }
 
 func (info *recordInfo) AddBoolField(name string, source string) string {
-	return info.addField(name, source, 1, 0, BoolType, 1)
+	return info.addField(name, source, 1, 0, BoolType, 1, 0)
 }
 
 func (info *recordInfo) AddInt16Field(name string, source string) string {
-	return info.addField(name, source, 2, 0, Int16Type, 3)
+	return info.addField(name, source, 2, 0, Int16Type, 2, 1)
 }
 
 func (info *recordInfo) AddInt32Field(name string, source string) string {
-	return info.addField(name, source, 4, 0, Int32Type, 5)
+	return info.addField(name, source, 4, 0, Int32Type, 4, 1)
 }
 
 func (info *recordInfo) AddInt64Field(name string, source string) string {
-	return info.addField(name, source, 8, 0, Int64Type, 9)
+	return info.addField(name, source, 8, 0, Int64Type, 8, 1)
 }
 
 func (info *recordInfo) AddFixedDecimalField(name string, source string, size int, precision int) string {
-	return info.addField(name, source, size, precision, FixedDecimalType, uintptr(size)+1)
+	return info.addField(name, source, size, precision, FixedDecimalType, uintptr(size), 1)
 }
 
 func (info *recordInfo) AddFloatField(name string, source string) string {
-	return info.addField(name, source, 4, 0, FloatType, 5)
+	return info.addField(name, source, 4, 0, FloatType, 4, 1)
 }
 
 func (info *recordInfo) AddDoubleField(name string, source string) string {
-	return info.addField(name, source, 8, 0, DoubleType, 9)
+	return info.addField(name, source, 8, 0, DoubleType, 8, 1)
 }
 
 func (info *recordInfo) AddStringField(name string, source string, size int) string {
-	return info.addField(name, source, size, 0, StringType, uintptr(size)+1)
+	return info.addField(name, source, size, 0, StringType, uintptr(size), 1)
 }
 
 func (info *recordInfo) AddWStringField(name string, source string, size int) string {
-	return info.addField(name, source, size, 0, WStringType, uintptr(size)+1)
+	return info.addField(name, source, size, 0, WStringType, uintptr(size), 1)
 }
 
 func (info *recordInfo) AddV_StringField(name string, source string, size int) string {
-	return info.addField(name, source, size, 0, V_StringType, 4)
+	return info.addField(name, source, size, 0, V_StringType, 4, 0)
 }
 
 func (info *recordInfo) AddV_WStringField(name string, source string, size int) string {
-	return info.addField(name, source, size, 0, V_WStringType, 4)
+	return info.addField(name, source, size, 0, V_WStringType, 4, 0)
 }
 
 func (info *recordInfo) AddDateField(name string, source string) string {
-	return info.addField(name, source, 10, 0, DateType, 11)
+	return info.addField(name, source, 10, 0, DateType, 10, 1)
 }
 
 func (info *recordInfo) AddDateTimeField(name string, source string) string {
-	return info.addField(name, source, 19, 0, DateTimeType, 20)
+	return info.addField(name, source, 19, 0, DateTimeType, 19, 1)
 }
 
 func (info *recordInfo) GetByteValueFrom(fieldName string, record unsafe.Pointer) (byte, error) {
@@ -205,19 +207,21 @@ func (info *recordInfo) GetDoubleValueFrom(fieldName string, record unsafe.Point
 	return *((*float64)(unsafe.Pointer(recordPtr))), nil
 }
 
-func (info *recordInfo) addField(name string, source string, size int, scale int, fieldType string, fixedLen uintptr) string {
+func (info *recordInfo) addField(name string, source string, size int, scale int, fieldType string, fixedLen uintptr, nullByteLen uintptr) string {
 	actualName := info.checkFieldName(name)
 	info.fields = append(info.fields, FieldInfo{
-		Name:      actualName,
-		Source:    source,
-		Size:      size,
-		Precision: scale,
-		Type:      fieldType,
-		location:  info.currentLen,
+		Name:        actualName,
+		Source:      source,
+		Size:        size,
+		Precision:   scale,
+		Type:        fieldType,
+		location:    info.currentLen,
+		fixedLen:    fixedLen,
+		nullByteLen: nullByteLen,
 	})
 	info.fieldNames[actualName] = info.numFields
 	info.numFields++
-	info.currentLen += fixedLen
+	info.currentLen += fixedLen + nullByteLen
 	return actualName
 }
 
